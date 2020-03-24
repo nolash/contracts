a620406ebc2830e287e3b7d9c8080d518f767774
nolash <dev@holbrook.no>
2020-03-24 21:09:58 +0100

    Add anchor token to network pathfinder

--- solidity/migrations/3_grassroots.js
+++ solidity/migrations/3_grassroots.js
@@ -28,7 +28,6 @@ module.exports = async function(deployer, network, accounts) {
 		let network = await deployer.deploy(BancorNetwork, registry.address);
 		let etherToken = await deployer.deploy(EtherToken, "Sarafu Ether token", "SFUETH");
 		let smartToken = await deployer.deploy(SmartToken, 'Sarafu', 'SFU', 18);
-		//let smartTokenB = await deployer.deploy(SmartTokenB, 'Sarafoo', 'SFO', 18);
 		let converterFactory = await deployer.deploy(BancorConverterFactory);
 		let features = await deployer.deploy(ContractFeatures);
 		let formula = await deployer.deploy(BancorFormula);
@@ -49,23 +48,10 @@ module.exports = async function(deployer, network, accounts) {
 
 		// fund the reserve ethertoken
 		let converterAddress = await converterFactory.createConverter(smartToken.address, registry.address, 0, etherToken.address, 25000);
-		//let converterAddressB = await converterFactory.createConverter(smartTokenB.address, registry.address, 0, etherToken.address, 25000);
 
 		let converter = await BancorConverter.at(converterAddress.logs[0].args._converter);
 		await converter.acceptOwnership();
-		//let converterB = await BancorConverter.at(converterAddressB.logs[0].args._converter);
-		//await converterB.acceptOwnership();
 
-		// give eth to converter and approve its use of the eth (? - why is the approve necessary?)
-		//let b = await converter.getConnectorBalance(etherToken.address);
-		//console.log("conn balance ether", b);
-		let s = await smartToken.totalSupply();
-		let e = await etherToken.totalSupply();
-		console.log("token supply", s, e);
-
-		//s = await smartTokenB.totalSupply();
-		//console.log("tokenB supply", s, e);
-		
 		await web3.eth.sendTransaction({from: accounts[0], to: etherToken.address, value: amount_initial_reserve});
 		await etherToken.transfer(converter.address, amount_initial_reserve_token);
 
@@ -73,30 +59,7 @@ module.exports = async function(deployer, network, accounts) {
 		await smartToken.transferOwnership(converter.address);
 		let t = await converter.acceptTokenOwnership();
 
-		//await smartTokenB.issue(accounts[1], amount_initial_minted_token);	
-		//await smartTokenB.transferOwnership(converterB.address);
-		//t = await converterB.acceptTokenOwnership();
-
-		e = await etherToken.totalSupply();
-		console.log("ether supply", e);
-		s = await smartToken.totalSupply();
-		o = await smartToken.owner();
-		console.log("token supply", s, e, o, converter.address);
-		let c = await converter.connectorTokenCount();
-		console.log("reserve", c);
-
-		//s = await smartTokenB.totalSupply();
-		//o = await smartTokenB.owner();
-		//console.log("tokenB supply", s, e, o, converterB.address);
-		//c = await converterB.connectorTokenCount();
-		//console.log("reserve", c);
-
-		//console.log("isvalid", await converterRegistry.isConverterValid(converter.address), await converterRegistry.isConverterValid(converterB.address));
-		console.log("isvalid", await converterRegistry.isConverterValid(converter.address));
-
 		let r = await converterRegistry.addConverter(converter.address);
-		console.log("converter added to registry", r);
-		//r = await converterRegistry.addConverter(converterB.address);
-		//console.log("converterB added to registry", r);
+		await networkPathFinder.setAnchorToken(etherToken.address)
 	});
 }
