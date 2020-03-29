61c82b2ea9de5289208ce92ba2e565f2e547256f
nolash <dev@holbrook.no>
2020-03-29 21:12:22 +0200

    Merge payment and converter deploy to dev-only section in migrate 3

--- solidity/migrations/3_grassroots.js
+++ solidity/migrations/3_grassroots.js
@@ -13,6 +13,10 @@ let Whitelist = artifacts.require('Whitelist');
 
 let web3 = require('web3');
 
+let amount_initial_reserve = '10000000000000';
+let amount_initial_reserve_token = 1000000;
+let amount_initial_minted_token = 4000000;
+
 module.exports = async function(deployer, network, accounts) {
 	deployer.then(async () => {
 		// deploy all necessary contracts
@@ -50,10 +54,34 @@ module.exports = async function(deployer, network, accounts) {
 		// register the reserve token
 		await network.registerEtherToken(etherToken.address, true);
 
-		//let converterAddress = await converterFactory.createConverter(smartToken.address, registry.address, 0, etherToken.address, 25000);
-		//let converter = await BancorConverter.at(converterAddress.logs[0].args._converter);
-		//await converter.acceptOwnership();
-
 		await networkPathFinder.setAnchorToken(etherToken.address)
+		
+		if (network == 'development') {
+			console.log(">>>> deploying with account " + accounts[0])
+			let smartToken = await SmartToken.deployed();
+
+			let converterAddress = await converterFactory.createConverter(smartToken.address, registry.address, 0, etherToken.address, 25000);
+			let converter = await BancorConverter.at(converterAddress.logs[0].args._converter);
+			await converter.acceptOwnership();
+
+			let tx = await web3.eth.sendTransaction({from: accounts[0], to: etherToken.address, value: amount_initial_reserve});
+			console.log('sent', tx);
+
+			await etherToken.transfer(converter.address, amount_initial_reserve_token);
+			console.log('transfer');
+
+			await smartToken.issue(accounts[0], amount_initial_minted_token);
+			console.log('issue');
+
+			await smartToken.transferOwnership(converter.address);
+			console.log('transferowner');
+
+			let t = await converter.acceptTokenOwnership();
+			console.log('acceptowner');
+
+			let r = await converterRegistry.addConverter(converter.address);
+			console.log('addconverter');
+		}
+
 	});
 }
