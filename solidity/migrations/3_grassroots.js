3e9a02cce547fa04be91f52a2cd56c3f7f62681e
nolash <dev@holbrook.no>
2020-03-29 21:30:47 +0200

    Remove accidental overwrite of network var

--- solidity/migrations/3_grassroots.js
+++ solidity/migrations/3_grassroots.js
@@ -7,15 +7,18 @@ let BancorConverterRegistry = artifacts.require('BancorConverterRegistry');
 let BancorConverterRegistryData = artifacts.require('BancorConverterRegistryData');
 let EtherToken = artifacts.require('EtherToken');
 let SmartTokenController = artifacts.require('SmartTokenController');
+let SmartToken = artifacts.require('SmartToken');
 let BancorFormula = artifacts.require('BancorFormula');
 let ContractFeatures = artifacts.require('ContractFeatures');
 let Whitelist = artifacts.require('Whitelist');
 
-let web3 = require('web3');
+let w3 = require('web3');
 
 let amount_initial_reserve = '10000000000000';
 let amount_initial_reserve_token = 1000000;
-let amount_initial_minted_token = 4000000;
+let reserve_multiplier = 4;
+let reserve_ratio = 1000000 / reserve_multiplier;
+let amount_initial_minted_token = amount_initial_reserve_token * reserve_multiplier;
 
 module.exports = async function(deployer, network, accounts) {
 	deployer.then(async () => {
@@ -24,63 +27,56 @@ module.exports = async function(deployer, network, accounts) {
 		deployer.link(ContractRegistry, [BancorConverterRegistry, BancorConverterRegistryData]);
 		let converterRegistry = await deployer.deploy(BancorConverterRegistry, registry.address);
 		let converterRegistryData = await deployer.deploy(BancorConverterRegistryData, registry.address);
-		let network = await deployer.deploy(BancorNetwork, registry.address);
+		let converterNetwork = await deployer.deploy(BancorNetwork, registry.address);
 		let etherToken = await deployer.deploy(EtherToken, "Grassroots XDAI Reserve Token", "XDAIGR");
 		let converterFactory = await deployer.deploy(BancorConverterFactory);
 		let features = await deployer.deploy(ContractFeatures);
 		let formula = await deployer.deploy(BancorFormula);
-		let networkPathFinder = await deployer.deploy(BancorNetworkPathFinder, registry.address);
+		let converterNetworkPathFinder = await deployer.deploy(BancorNetworkPathFinder, registry.address);
 
 
 		// set up the registry
-		name = web3.utils.asciiToHex('ContractRegistry')
+		name = w3.utils.asciiToHex('ContractRegistry')
 		await registry.registerAddress(name, registry.address);
 		console.log("contractregistry has " + name);
-		name = web3.utils.asciiToHex('BancorNetwork')
-		await registry.registerAddress(name, network.address);
-		name = web3.utils.asciiToHex('ContractFeatures')
+		name = w3.utils.asciiToHex('BancorNetwork')
+		await registry.registerAddress(name, converterNetwork.address);
+		name = w3.utils.asciiToHex('ContractFeatures')
 		await registry.registerAddress(name, features.address);
-		name = web3.utils.asciiToHex('BancorConverterRegistry')
+		name = w3.utils.asciiToHex('BancorConverterRegistry')
 		await registry.registerAddress(name, converterRegistry.address);
-		name = web3.utils.asciiToHex('BancorConverterFactory')
+		name = w3.utils.asciiToHex('BancorConverterFactory')
 		await registry.registerAddress(name, converterFactory.address);
-		name = web3.utils.asciiToHex('BancorConverterRegistryData')
+		name = w3.utils.asciiToHex('BancorConverterRegistryData')
 		await registry.registerAddress(name, converterRegistryData.address);
-		name = web3.utils.asciiToHex('BancorNetworkPathFinder')
-		await registry.registerAddress(name, networkPathFinder.address);
-		name = web3.utils.asciiToHex('BancorFormula')
+		name = w3.utils.asciiToHex('BancorNetworkPathFinder')
+		await registry.registerAddress(name, converterNetworkPathFinder.address);
+		name = w3.utils.asciiToHex('BancorFormula')
 		await registry.registerAddress(name, formula.address);
 
 		// register the reserve token
-		await network.registerEtherToken(etherToken.address, true);
+		await converterNetwork.registerEtherToken(etherToken.address, true);
 
-		await networkPathFinder.setAnchorToken(etherToken.address)
-		
+		await converterNetworkPathFinder.setAnchorToken(etherToken.address)
+	
+		console.log('network', network);
 		if (network == 'development') {
-			console.log(">>>> deploying with account " + accounts[0])
-			let smartToken = await SmartToken.deployed();
+			let smartToken = await deployer.deploy(SmartToken, 'SARAFU', 'SFU', 18);
 
-			let converterAddress = await converterFactory.createConverter(smartToken.address, registry.address, 0, etherToken.address, 25000);
+			let converterAddress = await converterFactory.createConverter(smartToken.address, registry.address, 0, etherToken.address, reserve_ratio);
 			let converter = await BancorConverter.at(converterAddress.logs[0].args._converter);
 			await converter.acceptOwnership();
 
 			let tx = await web3.eth.sendTransaction({from: accounts[0], to: etherToken.address, value: amount_initial_reserve});
-			console.log('sent', tx);
-
 			await etherToken.transfer(converter.address, amount_initial_reserve_token);
-			console.log('transfer');
 
 			await smartToken.issue(accounts[0], amount_initial_minted_token);
-			console.log('issue');
 
 			await smartToken.transferOwnership(converter.address);
-			console.log('transferowner');
 
 			let t = await converter.acceptTokenOwnership();
-			console.log('acceptowner');
 
 			let r = await converterRegistry.addConverter(converter.address);
-			console.log('addconverter');
 		}
 
 	});
